1ad36522669eab525aa5997a80317cafff96bd26
adam <adam.marek.pietras@gmail.com>
2019-12-11 15:38:59 +0100

    prepared sqlimplementaiton and corrected pssqljdbc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,9 @@ import SQL.PostgreSQLJDBC;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Dupka");
+        System.out.println("Dupddddka");
         PostgreSQLJDBC postgreSQLJDBC = new PostgreSQLJDBC();
         postgreSQLJDBC.connectWithDB();
+
     }
 }
