ccfbf47ca5b624d001b2faaa4fe4d8625e1011e2
byrtus <byrtus@wp.pl>
2019-12-12 13:54:15 +0100

    Refactor Database scripts

--- src/main/resources/CyberStoreDB_public_student.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO public.student (id, coins, user_id) VALUES (1, 10, 1);
-INSERT INTO public.student (id, coins, user_id) VALUES (2, 11, 2);
-INSERT INTO public.student (id, coins, user_id) VALUES (3, 12, 3);
-INSERT INTO public.student (id, coins, user_id) VALUES (4, 14, 4);
---
+++ src/main/resources/CyberStoreDB_public_students.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.students (id, coins, user_id) VALUES (1, 10, 1);
+INSERT INTO public.students (id, coins, user_id) VALUES (2, 11, 2);
+INSERT INTO public.students (id, coins, user_id) VALUES (3, 12, 3);
+INSERT INTO public.students (id, coins, user_id) VALUES (4, 14, 4);
