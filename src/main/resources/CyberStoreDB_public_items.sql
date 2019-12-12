80ee81e4e658f79c2925ef8cd09d513c41ceb4e0
byrtus <byrtus@wp.pl>
2019-12-12 13:12:43 +0100

    Add script

--- src/main/resources/CyberStoreDB_public_items.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO public.items (id, name, price, description, is_active) VALUES (1, 'accustrike', 124, 'nerf accustraike series falconfire', null);
-INSERT INTO public.items (id, name, price, description, is_active) VALUES (2, 'micro shots', 20, 'nerf micro shots z', null);
-INSERT INTO public.items (id, name, price, description, is_active) VALUES (3, 'roughcut 2x4', 215, 'nerf roughcut 2x4 n-strike elite', null);
-INSERT INTO public.items (id, name, price, description, is_active) VALUES (4, 'whiteboard marker', 6, 'staedtler whiteboard marker lumocolor dry safe - blue', null);
-INSERT INTO public.items (id, name, price, description, is_active) VALUES (5, 'maxiflo whiteboard marker', 4, 'pentel maxiflo white board marker push button-type liquid ink type - black', null);
-INSERT INTO public.items (id, name, price, description, is_active) VALUES (6, 'maxfilo whiteboard marker', 4, 'pentel maxfilo white board marker push button type liquid ink type - green', null);
