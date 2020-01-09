26f642b2c086182896f3fed6b988b9df6e013439
byrtus <byrtus@wp.pl>
2020-01-09 10:14:14 +0100

    Add database scripts

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import SQL.PostgreSQLJDBC;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Dupddddka");
+        System.out.println("Test");
         PostgreSQLJDBC postgreSQLJDBC = new PostgreSQLJDBC();
         postgreSQLJDBC.connect();
 
