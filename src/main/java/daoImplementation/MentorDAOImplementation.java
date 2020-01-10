39480a1268a5b4cc099d6a239d3ba1b754cb0ae9
adam <adam.marek.pietras@gmail.com>
2020-01-10 20:28:20 +0100

    methods add, delete, get in MentorDaoImpl added and refactored, ready for tests

--- src/main/java/daoImplementation/MentorDAOImplementation.java
+++ src/main/java/daoImplementation/MentorDAOImplementation.java
@@ -1,7 +1,189 @@
 package daoImplementation;
 
+import SQL.PostgreSQLJDBC;
+import interfaces.CRUD;
 import interfaces.MentorDAO;
+import models.Student;
+import models.User;
 
-public class MentorDAOImplementation {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class MentorDAOImplementation{
+    PostgreSQLJDBC postgreSQLJDBC = new PostgreSQLJDBC();
+    PreparedStatement preparedStatement = null;
+    ResultSet resultSet = null;
+    Scanner scanner = new Scanner(System.in);
+
+
+    public void addStudent(Student student) {
+        String orderToSqlUsers = "INSERT INTO user_details (login, password, first_name, last_name) VALUES (?,?,?,?)";
+        String orderToSqlDetails = "INSERT INTO users (user_type_id, is_active, user_details_id) VALUES (?,?,?)";
+
+        try {
+            preparedStatement = postgreSQLJDBC.connect().prepareStatement(orderToSqlUsers);
+            preparedStatement.setString(1, student.getLogin());
+            preparedStatement.setString(2, student.getPassword());
+            preparedStatement.setString(3, student.getFirstname());
+            preparedStatement.setString(4, student.getLastname());
+            int row = preparedStatement.executeUpdate();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            int userTypeId = 1;
+            boolean setIsActive = true;
+            int userDetailId = 1;
+            preparedStatement = postgreSQLJDBC.connect().prepareStatement(orderToSqlDetails);
+            preparedStatement.setInt(1, userTypeId);
+            preparedStatement.setBoolean(2,setIsActive);
+            preparedStatement.setInt(3, userDetailId);
+            int row = preparedStatement.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void editStudent(Student student) {
+        String orderToSqlDetails = ("UPDATE user_details SET login = ?, password = ?, first_name = ?, last_name = ? WHERE id = ?");
+        String orderToSqlUsers = ("UPDATE user SET is_active = ?");
+        //TODO Bartek
+        try {
+            preparedStatement = postgreSQLJDBC.connect().prepareStatement(orderToSqlDetails);
+            System.out.println("Insert new student login: ");
+            String login = scanner.next();
+            System.out.println("Insert new student password: ");
+            String password = scanner.next();
+            System.out.println("Insert new student name: ");
+            String firstName = scanner.next();
+            System.out.println("Insert new student surname: ");
+            String lastName = scanner.next();
+
+
+            preparedStatement.setString(1, login);
+            preparedStatement.setString(2, password);
+            preparedStatement.setString(3, firstName);
+            preparedStatement.setString(4, lastName);
+
+            int row = preparedStatement.executeUpdate();
+
+        }catch (Exception e){
+            System.out.println(e);
+        }finally {
+            try {
+                preparedStatement.close();
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            System.out.println("Set user category: press 1 to add as student" +
+                    "press 2 to add as mentor" +
+                    "press 3 to add as creep");
+            int userTypeId = scanner.nextInt();
+            System.out.println("set if student is active or unactive");
+            boolean setIsActive = scanner.nextBoolean();
+            int userDetailId = 1;
+            preparedStatement = postgreSQLJDBC.connect().prepareStatement(orderToSqlUsers);
+            preparedStatement.setInt(1, userTypeId);
+            preparedStatement.setBoolean(2,setIsActive);
+            preparedStatement.setInt(3, userDetailId);
+            int row = preparedStatement.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void deleteStudent(int id) {
+        String orderToSql = "DELETE FROM user_details WHERE id = ?";
+        String orderToSqlUser = "DELETE FROM users WHERE id = ?";
+        try {
+            preparedStatement = postgreSQLJDBC.connect().prepareStatement(orderToSql);
+            preparedStatement.setInt(1, id);
+            int row = preparedStatement.executeUpdate();
+        }catch (Exception e){
+            System.out.println(e);
+        }finally {
+            try {
+                preparedStatement.close();
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+        try {
+            preparedStatement = postgreSQLJDBC.connect().prepareStatement(orderToSqlUser);
+            preparedStatement.setInt(1, id);
+            int row = preparedStatement.executeUpdate();
+        }catch (Exception e){
+            System.out.println(e);
+        }finally {
+            try {
+                preparedStatement.close();
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+    }
+    public List<User> getStudentList() {
+        String orderToSql = "SELECT * FROM users " +
+                "join user_details " +
+                "on users.user_details_id = user_details.id WHERE users.user_type_id = ?";
+        List<User> studentList = new ArrayList<>();
+        try {
+            int studentTypeId = 1;
+            preparedStatement = postgreSQLJDBC.connect().prepareStatement(orderToSql);
+            preparedStatement.setInt(1, studentTypeId);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String login = resultSet.getString("login");
+                String password = resultSet.getString("password");
+                int userTypeId = resultSet.getInt("user_type_id");
+                String firstName = resultSet.getString("first_name");
+                String lastName = resultSet.getString("last_name");
+                User user = new Student(id, login, password, userTypeId, firstName, lastName);
+                studentList.add(user);
+
+                System.out.println(id + "| " + login + "| " + password + "| " + userTypeId + "| " + firstName + "| " + lastName);
+
+            }
+            preparedStatement.executeQuery();
+        } catch (SQLException e) {
+            System.out.println(e);
+        } finally {
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+
+            }
+
+        }
+        return studentList;
+    }
 }
