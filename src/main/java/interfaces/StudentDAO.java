cd13f006b46440d3c13f0a78ae16d8aacede2754
propertyofkeow <marta.sibielak92@gmail.com>
2020-01-11 17:04:35 +0100

    changes methods in userDao

--- src/main/java/interfaces/StudentDAO.java
+++ src/main/java/interfaces/StudentDAO.java
@@ -6,8 +6,15 @@ import java.util.List;
 
 public interface StudentDAO{
 
-    public void addStudent(String login, String password, String firstName, String lastName);
-    public List<User> extractStudent();
-    public void editStudent(int userId);
-    public void removeStudent(int userId);
+    public void showItems();
+    public void showCoincubatos();
+    public void showUserItems();
+    public void showUserCoins();
+    public void showUserQuests();
+
+
+//    public void addStudent(String login, String password, String firstName, String lastName);
+//    public List<User> extractStudent();
+//    public void editStudent(int userId);
+//    public void removeStudent(int userId);
 }
