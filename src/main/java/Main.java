c174bfbe7c8191693cf44ff66b7eb831b3b723cc
adam <adam.marek.pietras@gmail.com>
2020-01-14 14:56:06 +0100

    finished QuestController

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import SQL.PostgreSQLJDBC;
 import controllers.QuestController;
 import daoImplementation.ItemDAOImplementation;
 import models.Item;
+import models.Quest;
 
 public class Main {
     public static void main(String[] args) {
