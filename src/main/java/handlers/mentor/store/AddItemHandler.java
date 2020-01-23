88b5b3762b067541cb98c345168fe387a0c6d9a7
adam <adam.marek.pietras@gmail.com>
2020-01-23 20:24:08 +0100

    working getallitems method

--- src/main/java/handlers/mentor/store/AddItemHandler.java
+++ src/main/java/handlers/mentor/store/AddItemHandler.java
@@ -2,12 +2,74 @@ package handlers.mentor.store;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import models.Item;
+import models.Quest;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
+import services.ItemService;
+import services.QuestService;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddItemHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
 
+        ItemService itemService = new ItemService();
+        String method = httpExchange.getRequestMethod();
+        System.out.println(method);
+
+        String response = "";
+
+
+        if (method.equals("GET")) {
+            JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/mentor/add-item.twig");
+            JtwigModel model = JtwigModel.newModel();
+
+            response = template.render(model);
+        }
+
+        if (method.equals("POST")) {
+            InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody(), "utf-8");
+            BufferedReader br = new BufferedReader(isr);
+            String formData = br.readLine();
+
+            Map inputs = parseFormData(formData);
+
+            JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/mentor/add-item.twig");
+            JtwigModel model = JtwigModel.newModel();
+            response = template.render(model);
+
+            System.out.println(inputs.get("name").toString() + inputs.get("price").toString() + inputs.get("description").toString());
+            itemService.addNewItem(new Item(inputs.get("name").toString(), Integer.parseInt(inputs.get("price").toString()), inputs.get("description").toString(),  true));
+
+//            httpExchange.getResponseHeaders().set("Location", "/mentor/add-item" );
+//            httpExchange.sendResponseHeaders(303,0);
+        }
+        httpExchange.sendResponseHeaders(200, response.length());
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+
+
+    private Map<String, String> parseFormData(String formData) {
+        Map<String, String> map = new HashMap<String, String>();
+        String[] pairs = formData.split("&");
+        for(String pair : pairs){
+            String[] keyValue = pair.split("=");
+            String value = null;
+            try {
+                value = new URLDecoder().decode(keyValue[1], "UTF-8");
+                map.put(keyValue[0], value);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        return map;
     }
 }
+
