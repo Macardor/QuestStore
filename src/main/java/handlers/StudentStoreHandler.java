a97603e3ff148a41f6193d25ea5b914b97876a36
Maciek <macardorpl@gmail.com>
2020-01-23 13:57:56 +0100

    Store layout and connection

--- src/main/java/handlers/StudentStoreHandler.java
+++ src/main/java/handlers/StudentStoreHandler.java
@@ -2,21 +2,27 @@ package handlers;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import daoImplementation.ItemDAOImplementation;
+import models.Item;
 import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.List;
 
 public class StudentStoreHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
 
         String method = httpExchange.getRequestMethod();
+        ItemDAOImplementation itemDAOImplementation = new ItemDAOImplementation();
+        List<Item> itemsList = itemDAOImplementation.getItemsList();
 
         if (method.equals("GET")){
             JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/student/store.twig");
             JtwigModel model = JtwigModel.newModel();
+            model.with("itemsList", itemsList);
             String response = template.render(model);
 
             httpExchange.sendResponseHeaders(200, response.length());
