f2f194015606077899a1f49c4dacea018bbf04df
Maciek <macardorpl@gmail.com>
2020-01-23 15:08:56 +0100

    Cookie check in Student => quest

--- src/main/java/handlers/StudentQuestsHandler.java
+++ src/main/java/handlers/StudentQuestsHandler.java
@@ -4,7 +4,10 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import daoImplementation.QuestDAOImplementation;
 import daoImplementation.StudentDAOImplementation;
+import helpers.CookieHandler;
 import models.Quest;
+import models.Student;
+import models.User;
 import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
 
@@ -13,13 +16,20 @@ import java.io.OutputStream;
 import java.util.List;
 
 public class StudentQuestsHandler implements HttpHandler {
+    User user = null;
+    CookieHandler cookieHandler = new CookieHandler();
+
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
+        user = cookieHandler.cookieChecker(httpExchange);
+        if(user == null || user.getUserType() != 1){
+            httpExchange.getResponseHeaders().set("Location", "/login");
+            httpExchange.sendResponseHeaders(303, 0);
+        }
 
         String method = httpExchange.getRequestMethod();
         QuestDAOImplementation questDAOImplementation = new QuestDAOImplementation();
-        StudentDAOImplementation studentDAOImplementation = new StudentDAOImplementation();
-        List<Quest> questsList = questDAOImplementation.getAllQuestsNotDoneByStudent(studentDAOImplementation.getStudentByUserId(1));
+        List<Quest> questsList = questDAOImplementation.getAllQuestsNotDoneByStudent((Student) user);
 
         if (method.equals("GET")){
             JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/student/quests.twig");
